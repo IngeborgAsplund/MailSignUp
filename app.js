b60cecf96282c3dd9c223c653db1d9aacfb96815
IngeborgAsplund <ingeborg.m.asplund@gmail.com>
2022-10-25 18:33:21 +0200

    get and post method added

--- app.js
+++ app.js
@@ -3,7 +3,22 @@ const bodyParser = require('body-parser');
 const request = require ('request');
 
 const app = express();
-app.use(bodyParser);
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+
+
+app.get("/", function(req, res){
+
+  res.sendFile(__dirname+"/signup.html");
+});
+
+app.post("/", function(req, res){
+  var firstname = req.body.firstname;
+  var lastname = req.body.lastname;
+  var email = req.body.mail;
+  console.log(firstname +" " + lastname + " " + email);
+});
 
 app.listen(3000, function(){
   console.log("up and running on port 3000")

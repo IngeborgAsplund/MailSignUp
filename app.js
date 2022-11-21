4f1fab82d4744291e2f92a404c1e88c19bbfee81
IngeborgAsplund <ingeborg.m.asplund@gmail.com>
2022-11-21 16:00:46 +0100

    Update app.js
    started to add the necessary info to post to mailchimp api

--- app.js
+++ app.js
@@ -17,9 +17,23 @@ app.post("/", function(req, res){
   var firstname = req.body.firstname;
   var lastname = req.body.lastname;
   var email = req.body.mail;
-  console.log(firstname +" " + lastname + " " + email);
+
+  var data ={
+    members:[
+      {
+        email_address:email,
+        status:"subscribed",
+        merge_fields:{
+          
+        }
+      }
+    ]
+  }
 });
 
 app.listen(3000, function(){
   console.log("up and running on port 3000")
 });
+
+//0703065e3a8d70b6609cd7152f971a24-us21 api key mailchimp
+//d93e611a90 audience id

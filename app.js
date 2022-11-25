80bacab96ec5fc2050bdc6b279187b16ec8ceed7
IngeborgAsplund <ingeborg.m.asplund@gmail.com>
2022-11-25 13:20:29 +0100

    finished

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const request = require ('request');
+const https = require ('https');
 
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
@@ -14,24 +15,48 @@ app.get("/", function(req, res){
 });
 
 app.post("/", function(req, res){
-  var firstname = req.body.firstname;
-  var lastname = req.body.lastname;
-  var email = req.body.mail;
+  const firstname = req.body.firstname;
+  const lastname = req.body.lastname;
+  const email = req.body.mail;
 
-  var data ={
+  const data ={
     members:[
       {
         email_address:email,
         status:"subscribed",
         merge_fields:{
-          
+          FNAME: firstname,
+          LNAME: lastname
         }
       }
     ]
-  }
-});
+  };
+  const jsonData = JSON.stringify(data);
 
-app.listen(3000, function(){
+  const url = "https://us21.api.mailchimp.com/3.0/lists/d93e611a90";
+  const options = {
+    method:"POST",
+    auth:"udemycourse:0703065e3a8d70b6609cd7152f971a24-us21"
+  };
+
+  const request = https.request(url, options, function(response){
+    response.on("data",function(data){
+      const status = response.statusCode;
+      if(status === 200){
+        res.sendFile(__dirname+"/success.html");
+      }
+      else{
+        res.sendFile(__dirname+"/failure.html");
+      }
+    });
+  });
+  request.write(jsonData);
+  request.end();
+});
+app.post("/failure",function(req,res){
+  res.redirect("/");
+});
+app.listen(process.env.PORT ||3000, function(){
   console.log("up and running on port 3000")
 });
 
